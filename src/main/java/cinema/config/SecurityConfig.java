58cdf8ad6328b981a028afc4e6cc74f1487c4098
MarkGodnyi <libertyinprogresslife@gmail.com>
2023-08-31 13:22:43 +0200

    added implementation of cinema-app project

---
+++ src/main/java/cinema/config/SecurityConfig.java
@@ -0,0 +1,48 @@
+package cinema.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final UserDetailsService userDetailsService;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public SecurityConfig(UserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
+        this.userDetailsService = userDetailsService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Autowired
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
+    }
+
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/register").permitAll()
+                .antMatchers(HttpMethod.POST,
+                        "/cinema-halls", "/movies").hasRole("ADMIN")
+                .antMatchers(HttpMethod.GET,
+                        "/cinema-halls", "/movies", "/movie-sessions/available")
+                .hasAnyRole("USER", "ADMIN")
+                .antMatchers("/movie-sessions/**", "/users/**").hasRole("ADMIN")
+                .antMatchers("/orders/**", "/shopping-carts/**").hasRole("USER")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .permitAll()
+                .and()
+                .httpBasic()
+                .and()
+                .csrf().disable();
+    }
+}
